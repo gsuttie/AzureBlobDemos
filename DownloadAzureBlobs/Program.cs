9380fb1db8d2342d21df2f20ecc3f146ea1b96c2
Gregor Suttie <gsuttie2001@hotmail.com>
2018-01-05 18:45:48 +0000

    first commit

---
+++ DownloadAzureBlobs/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DownloadAzureBlobs
+{
+    class Program
+    {
+        const string StorageAccountName = "gsazurecsharp";
+        const string StorageAccountKey = "871nNIZfCqMfbq/xmX2ipA9VPW0zbfn7jr1E9MlfW39wlBSvP0VyQMbdsABMkvJa3AwsM7fQ7X5gzoJhxUh2lw==";
+
+        static void Main(string[] args)
+        {
+            var storageAccount = new CloudStorageAccount(new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(StorageAccountName, StorageAccountKey), true);
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference("gsazuerecontainer");
+
+            var blobs = container.ListBlobs();
+
+            DownloadAzureBlobs(blobs);
+
+            Console.WriteLine("Completed");
+        }
+
+        private static void DownloadAzureBlobs(IEnumerable<IListBlobItem> blobs)
+        {
+            foreach (var blob in blobs)
+            {
+                if (blob is CloudBlockBlob blockblob)
+                {
+                    blockblob.DownloadToFile(blockblob.Name, FileMode.Create);
+                    Console.WriteLine("blob name is {0}", blockblob.Name);
+                }
+                else if (blob is CloudBlobDirectory blobDirectory)
+                {
+                    Directory.CreateDirectory(blobDirectory.Prefix);
+                    Console.WriteLine("Create Directory {0}", blobDirectory.Prefix);
+
+                    DownloadAzureBlobs(blobDirectory.ListBlobs());
+                }
+            }
+        }
+    }
+}
---
+++ gsAzureConsoleAppStorageAccount/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.IO;
+
+namespace UploadAzureBlobs
+{
+    class Program
+    {
+        const string StorageAccountName = "gsazurecsharp";
+        const string StorageAccountKey = "871nNIZfCqMfbq/xmX2ipA9VPW0zbfn7jr1E9MlfW39wlBSvP0VyQMbdsABMkvJa3AwsM7fQ7X5gzoJhxUh2lw==";
+        const string FolderPath = @"C:\code\gsazurecsharpdocs";
+
+        static void Main(string[] args)
+        {
+            var storageAccount = new CloudStorageAccount(new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(StorageAccountName, StorageAccountKey), true);
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference("gsazuerecontainer");
+            container.CreateIfNotExists();
+
+            container.SetPermissions(new Microsoft.WindowsAzure.Storage.Blob.BlobContainerPermissions()
+            {
+                PublicAccess = Microsoft.WindowsAzure.Storage.Blob.BlobContainerPublicAccessType.Blob
+            });
+
+            foreach (var filepath in Directory.GetFiles(FolderPath, "*.*", SearchOption.AllDirectories))
+            {
+                var blob = container.GetBlockBlobReference(filepath);
+                blob.UploadFromFile(filepath);
+
+                Console.WriteLine("Upload {0}", filepath);
+            }
+
+            Console.WriteLine("Completed");
+        }
+    }
+}
